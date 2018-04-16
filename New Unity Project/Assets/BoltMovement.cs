0b5f1f457abe36942925208baca8b8db6a6d23ab
Jeremy Zoitas <jezz331@gmail.com>
2018-04-16 12:31:21 +1000

    Got a basic shooting function to work on a button press which destroys on tagged enemies, created some temp prefabs to test shooting.

---
+++ New Unity Project/Assets/BoltMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoltMovement : MonoBehaviour {
+	public float m_fSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * m_fSpeed * Time.deltaTime;
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Enemy")
+		{
+			Destroy(gameObject);
+			Destroy(collision.gameObject);
+		}
+	}
+}
---
+++ New Unity Project/Assets/ShootingScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour {
+
+	[SerializeField]
+	public GameObject m_Player;
+	[SerializeField]
+	public GameObject m_CrossBowBoltPrefab;
+	[SerializeField]
+	public GameObject m_CrossBow;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Shoot()
+	{
+		Vector3 fwd = m_Player.transform.TransformDirection(Vector3.forward);
+
+		if(Physics.Raycast(m_Player.transform.position, fwd, 10))
+		{
+			GameObject CrossBowBolt = (GameObject)Instantiate(m_CrossBowBoltPrefab, m_Player.transform.position, transform.rotation);
+		}
+	}
+}
