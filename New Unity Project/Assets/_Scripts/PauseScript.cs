5be646dc40020de5af6453f803875a21770d8733
Jeremy Zoitas <jezz331@gmail.com>
2018-04-18 09:39:37 +1000

    Pause added.

---
+++ New Unity Project/Assets/_Scripts/PauseScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour {
+
+	public bool paused;
+	public GameObject PausePanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void PauseGame(bool Paused)
+	{
+		if (Paused)
+		{
+			PausePanel.SetActive(true);
+			Time.timeScale = 0;
+		}
+		else
+		{
+			PausePanel.SetActive(false);
+			Time.timeScale = 1;
+		}
+	}
+}
