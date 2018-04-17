91c2abae919130b555f174b78788a0e53abd5573
AIE\S169519 <zaneronaldtalbot@gmail.com>
2018-04-17 11:27:13 +1000

    Added Main Menu scene and menu manager script

---
+++ New Unity Project/Assets/_Scripts/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public void StartGame(){
+		SceneManager.LoadScene (1);
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+
+}
