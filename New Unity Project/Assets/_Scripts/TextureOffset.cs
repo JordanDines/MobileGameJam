72e73b9b83ab5e5153a7421a77ff88345a549109
AIE\S169519 <zaneronaldtalbot@gmail.com>
2018-04-17 10:48:27 +1000

    Added scene set dressing, added texture offset script, created prefabs from environment meshes

---
+++ New Unity Project/Assets/_Scripts/TextureOffset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureOffset : MonoBehaviour {
+
+	public float scrollSpeed = 0.5f;
+	public Renderer rend;
+
+	void Start(){
+		rend = GetComponent<Renderer> ();
+	}
+
+	void Update () {
+		float offset = Time.time * scrollSpeed;
+		rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+	}
+}
